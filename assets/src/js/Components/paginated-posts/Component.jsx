059fd6f6caa2e71cbc56b263ca006efa47f6927c
Adriana Hernández R <adriana.hernandez@vass.es>
2024-05-24 14:31:23 +0200

    move postItem html to a separated PostCard component in paginated-posts block

--- assets/src/js/Components/paginated-posts/Component.jsx
+++ assets/src/js/Components/paginated-posts/Component.jsx
@@ -3,8 +3,7 @@ import { __ } from "@wordpress/i18n";
 import Pagination from "./Pagination";
 import Skeleton from "./Skeleton";
 import GalleryModal from "../modal/GalleryModal";
-import { getThumbnailFromVideo } from "../../Services/CommonBlockFunctions";
-import VideoCover from "../common-frontend/VideoCover";
+import PostCard from "./PostCard";
 
 export default function Component({ attributes, is_edit_mode, blockProps }) {
   const { cpt, perPage, className, textForShowGalleryButton } = attributes;
@@ -79,62 +78,6 @@ export default function Component({ attributes, is_edit_mode, blockProps }) {
     });
   }
 
-  const postItem = (post, index) => {
-    const media = (post) => {
-      if (post.videoEmbed) {
-        const videoThumbnail = getThumbnailFromVideo(post.videoEmbed);
-        return (
-          <VideoCover videoThumbnail={videoThumbnail} imgAlt={post.title} />
-        )
-      }
-
-      if (post.image) {
-        return (
-          <img loading="lazy" width="350" height="197" srcSet={post.srcset ? post.srcset : ''} src={post.image} className="attachment-large size-large wp-post-image" alt={post.title} />
-        )
-      }
-
-      return (
-        <span href="#" className="">
-          <span className="skeleton__content"></span>
-        </span>
-      )
-    };
-
-    // if block has the gallery block style, then the post.url will be # and will add the class galleryModal to the li which will create and open the modal on click.
-    const postURL = hasGalleryStyle ? null : post.url;
-
-    return (
-      <li key={index} data-post-id={post.id} className="single-post-container" onClick={handlePostClick}>
-        <div className={`wp-block-latest-posts__featured-image`}>
-          <a href={postURL} aria-label={post.title} target="_self" rel="noopener noreferrer">
-            {media(post)}
-          </a>
-        </div>
-        <a className="wp-block-latest-posts__post-title" href={postURL} target="_self" rel="noopener noreferrer" >
-          {post.title}
-        </a>
-        <time dateTime={post.date} className="wp-block-latest-posts__post-date">
-          {post.date}
-        </time>
-        <div className="wp-block-latest-posts__post-excerpt">
-          {post.excerpt}
-        </div>
-        <div className="wp-block-buttons">
-          <div className="wp-block-button is-style-fill wp-block-button__clear-style--text-icon">
-            <a className="wp-block-button__link has-blue-color has-transparent-background-color has-text-color has-background has-link-color has-text-align-left wp-element-button" href={postURL} target="_self" rel="noopener noreferrer" >
-              <span>{__("More information", "gutenberg-blocks")}</span>
-              <img decoding="async" style={{ width: "43px" }} src="/wp-content/plugins/gutenberg-blocks/assets/build/img/icons/Plus-blue-1.svg" />
-              <svg viewBox="0 0 43 43" width="43" height="43" fill="none" xmlns="http://www.w3.org/2000/svg" ><rect width="43" height="43" rx="21.5" fill="#FFE000"></rect>
-                <path d="M22.5 16V20.5H27V22.5H22.5V27H20.5V22.5H16V20.5H20.5V16H22.5Z" fill="#005DA1" ></path>
-              </svg>
-            </a>
-          </div>
-        </div>
-      </li>
-    )
-  }
-
   const renderData = () => {
     if (isLoading) {
       return (
@@ -162,7 +105,7 @@ export default function Component({ attributes, is_edit_mode, blockProps }) {
     }
 
     return response?.posts.map((post, index) => (
-      postItem(post, index)
+      <PostCard key={index} post={post} hasGalleryStyle={hasGalleryStyle} handlePostClick={handlePostClick} />
     ))
   }
 
